3a8fb152f66d9fb896a24cfbef434aaecf4021fa
Kevin Hattingh <kevin.hattingh@sintrex.com>
2020-02-02 23:40:26 +0200

    Add event listeners to testArea input & resetButton. Add shell functions.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global constants
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
@@ -12,12 +13,25 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+  let textEntered = testArea.value;
 
+  console.log(textEntered);
+}
 
 // Start the timer:
+function start() {
+  let textEnteredLength = testArea.value.length;
 
+  console.log(textEnteredLength);
+}
 
 // Reset everything:
-
+function reset() {
+  console.log('Reset button has been pressed!');
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener('keypress', start, false);
+testArea.addEventListener('keyup', spellCheck, false);
+resetButton.addEventListener('click', reset, false);
