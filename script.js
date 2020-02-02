d34a684e02ed2e0bac24cd77e78dce36a44c359f
Kevin Hattingh <kevin.hattingh@sintrex.com>
2020-02-03 00:45:20 +0200

    Add functionality for the reset button

--- script.js
+++ script.js
@@ -66,7 +66,21 @@ function start() {
 
 // Reset everything:
 function reset() {
-  console.log('Reset button has been pressed!');
+  clearInterval(interval);
+
+  interval = null;
+
+  timer = [0, 0, 0, 0];
+
+  timerRunning = false;
+
+  testArea.value = '';
+
+  theTimer.innerHTML = '00:00:00';
+
+  testWrapper.style.borderColor = 'grey';
+
+  testArea.focus();
 }
 
 // Event listeners for keyboard input and the reset button:
